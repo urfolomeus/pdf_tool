632e25f0d0a16d10041afc405e5e698c226f372a
Alan Gardner <urfolomeus@gmail.com>
2024-09-29 22:51:46 +0100

    First pass at passing selection to backend

--- src/pdf/routes.py
+++ src/pdf/routes.py
@@ -1,4 +1,6 @@
-from flask import render_template, send_from_directory
+from operator import itemgetter
+
+from flask import jsonify, render_template, request, send_from_directory
 
 from . import pdf_blueprint
 
@@ -11,3 +13,16 @@ def index():
 @pdf_blueprint.route("/pdf/<filename>")
 def serve_pdf(filename):
     return send_from_directory("pdf/files/", filename)
+
+
+@pdf_blueprint.route("/pdf/selection", methods=["POST"])
+def process_selection():
+    data = request.json
+    id, x, y, width, height = itemgetter("id", "x", "y", "width", "height")(data)
+
+    print(f"ID: {id}, X: {x}, Y: {y}, Width: {width}, Height: {height}")
+
+    # hardwiring for now
+    value = "255"
+
+    return jsonify({"result": {"id": id, "value": value}})
