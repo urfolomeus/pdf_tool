730b70abefb4734c7af7b3f0d401116a8ffb0393
Alan Gardner <urfolomeus@gmail.com>
2024-10-02 17:36:43 +0100

    Rewrite JS starting with PDF rendering

--- src/static/js/app/index.js
+++ src/static/js/app/index.js
@@ -1,155 +1,43 @@
-const pageNum = 1; // At the moment, we only render the first page
-const pdfScale = 1;
+/* CONSTANTS ------------------------------------------------ */
 
-const pdfLayer = document.getElementById('pdf-layer');
-const selectionLayer = document.getElementById('selection-layer');
-const selectionCtx = selectionLayer.getContext('2d');
+const PDF_INITIAL_SCALE = 1;
+const PDF_PAGE_NUM = 1;
+const PDF_PATH = '/pdf/sample.pdf';
 
-let isSelecting = false;
-let selections = [];
-let startX, startY;
+
+/* GLOBAL VARIABLES ----------------------------------------- */
+
+let pdfPage;
+let scale = PDF_INITIAL_SCALE;
+
+
+/* DOM ELEMENTS --------------------------------------------- */
+
+const pdfCanvas = document.getElementById('pdf-layer');
+const selectionCanvas = document.getElementById('selection-layer');
+
+
+/* PDF SETUP ------------------------------------------------ */
 
 pdfjsLib.GlobalWorkerOptions.workerSrc = 'static/js/pdfjs/build/pdf.worker.mjs';
 
-pdfjsLib.getDocument(`/pdf/sample.pdf`).promise.then(pdf => {
-  pdf.getPage(pageNum).then(page => {
-    const viewport = page.getViewport({ scale: pdfScale });
-
-    pdfLayer.width = selectionLayer.width = viewport.width;
-    pdfLayer.height = selectionLayer.height = viewport.height;
-
-    page.render({
-      canvasContext: pdfLayer.getContext('2d'),
-      viewport: viewport
-    });
+// Load and render PDF
+pdfjsLib.getDocument(PDF_PATH).promise.then(pdf => {
+  pdf.getPage(PDF_PAGE_NUM).then(page => {
+    pdfPage = page;
+    renderPage();
   });
 });
 
-const startSelection = (e) => {
-  isSelecting = true;
-  [startX, startY] = [e.offsetX, e.offsetY];
-};
+// Render the PDF and selection layers
+const renderPage = () => {
+  const pdfViewport = pdfPage.getViewport({ scale });
 
-const updateSelection = (e) => {
-  if (!isSelecting) return;
-  const currentRect = getRect(e.offsetX, e.offsetY);
-  redrawSelectionCanvas(currentRect);
-};
+  pdfCanvas.width = selectionCanvas.width = pdfViewport.width;
+  pdfCanvas.height = selectionCanvas.height = pdfViewport.height;
 
-const endSelection = (e) => {
-  if (!isSelecting) return;
-
-  isSelecting = false;
-
-  const currentRect = getRect(e.offsetX, e.offsetY);
-  const currentSelection = { id: crypto.randomUUID(), ...currentRect };
-  selections.push(currentSelection);
-  processSelection(currentSelection);
-
-  redrawSelectionCanvas();
-};
-
-const getRect = (endX, endY) => {
-  return {
-    x: startX,
-    y: startY,
-    width: endX - startX,
-    height: endY - startY,
-    canvasHeight: selectionLayer.height,
-    canvasWidth: selectionLayer.width,
-  };
-};
-
-const processSelection = (selection) => {
-  fetch('/pdf/selection', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(selection)
-  })
-    .then(response => {
-      if (!response.ok) throw new Error('Failed to process selection');
-      return response.json()
-    })
-    .then(showResult)
-    .catch(error => console.error({ error }));
-};
-
-const showResult = ({ result: { id, value } }) => {
-  addInputToList(id, value);
-  updateSelectionValue(id, value);
-};
-
-const addInputToList = (id, value) => {
-  const resultInput = document.createElement('input', { type: 'text' });
-  resultInput.id = id;
-  resultInput.value = value;
-
-  const result = document.createElement('li');
-  result.appendChild(resultInput);
-
-  const results = document.querySelector('#results ul');
-  results.appendChild(result);
-
-  resultInput.addEventListener('input', (e) => {
-    updateSelectionValue(e.target.id, e.target.value)
+  pdfPage.render({
+    canvasContext: pdfCanvas.getContext('2d'),
+    viewport: pdfViewport
   });
-};
-
-const rebuildInputList = () => {
-  const results = document.querySelector('#results ul');
-  results.innerHTML = '';
-
-  selections.forEach(selection => {
-    addInputToList(selection.id, selection.value);
-  });
-};
-
-const updateSelectionValue = (id, value) => {
-  const selection = selections.find(selection => selection.id === id);
-  selection.value = value;
-  console.log({ selections });
-  updateTotal();
-};
-
-const updateTotal = () => {
-  const total = selections.reduce((acc, selection) => {
-    let value = parseInt(selection.value);
-
-    if (isNaN(value)) value = 0;
-
-    return acc + value;
-  }, 0);
-
-  document.getElementById('total').innerHTML = total;
-};
-
-const redrawSelectionCanvas = (currentRect = null) => {
-  selectionCtx.clearRect(0, 0, selectionLayer.width, selectionLayer.height);
-
-  selectionCtx.strokeStyle = 'red';
-  selectionCtx.lineWidth = 2;
-
-  selections.forEach(selection => {
-    selectionCtx.strokeRect(selection.x, selection.y, selection.width, selection.height);
-  });
-
-  if (currentRect) {
-    selectionCtx.strokeRect(currentRect.x, currentRect.y, currentRect.width, currentRect.height);
-  }
-};
-
-const clearSelections = () => {
-  selections = [];
-  rebuildInputList();
-  redrawSelectionCanvas();
-  updateTotal();
-};
-
-selectionLayer.addEventListener('mousedown', startSelection);
-selectionLayer.addEventListener('mousemove', updateSelection);
-selectionLayer.addEventListener('mouseup', endSelection);
-selectionLayer.addEventListener('mouseout', endSelection);
-
-document.getElementById('clear-selections').addEventListener('click', clearSelections);
+}
