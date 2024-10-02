c22fa3e81ec8933dbd66d2fc2e14315640af1300
Alan Gardner <urfolomeus@gmail.com>
2024-10-02 20:04:18 +0100

    Always return a number even if the captured text can't be parsed to an int
    This is for debugging purposes while we get the calculations sorted out.

--- src/pdf/utils/strategies/tesseract.py
+++ src/pdf/utils/strategies/tesseract.py
@@ -1,7 +1,16 @@
+import random
+
 import pytesseract
 
 
 def extract_text(cropped_image):
     text = pytesseract.image_to_string(cropped_image)
     print(f"Extracted text: {text}")
-    return text
+
+    # For now we want to ensure we return a number
+    try:
+        textAsNumber = int(text)
+    except ValueError:
+        textAsNumber = random.randint(12, 345)
+
+    return textAsNumber
