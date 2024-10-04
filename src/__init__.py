12ebeca7ba9e2ead378c8fa18cc5cb5d8dd4a96c
Alan Gardner <urfolomeus@gmail.com>
2024-10-04 16:56:13 +0100

    Reintroducing extractor strategies

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 
 from .pdf import pdf_blueprint
@@ -7,7 +9,14 @@ from .pdf.utils.strategies import easyocr_reader
 def create_app():
     app = Flask(__name__)
 
-    app.config["OCR_READER"] = easyocr_reader.EasyOCRReader()
+    extractor = os.getenv("EXTRACTOR")
+    print(f"Text extractor: {extractor}")
+
+    match extractor:
+        case "easyocr":
+            app.config["OCR_READER"] = easyocr_reader.EasyOCRReader()
+        case _:
+            raise ValueError(f"Invalid extractor: {extractor}")
 
     app.register_blueprint(pdf_blueprint)
 
