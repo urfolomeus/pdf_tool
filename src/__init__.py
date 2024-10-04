db33267531d89e94eda5e7458ae51158ec3b19a9
Alan Gardner <urfolomeus@gmail.com>
2024-10-04 17:14:24 +0100

    Adding paddle back in

--- src/__init__.py
+++ src/__init__.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask
 
 from .pdf import pdf_blueprint
-from .pdf.utils.strategies import easyocr_reader
+from .pdf.utils.strategies import easyocr_reader, paddle
 
 
 def create_app():
@@ -15,6 +15,8 @@ def create_app():
     match extractor:
         case "easyocr":
             app.config["OCR_READER"] = easyocr_reader.EasyOCRReader()
+        case "paddle":
+            app.config["OCR_READER"] = paddle.PaddleOCRReader()
         case _:
             raise ValueError(f"Invalid extractor: {extractor}")
 
--- src/pdf/utils/strategies/paddle.py
+++ src/pdf/utils/strategies/paddle.py
@@ -2,26 +2,25 @@ import numpy as np
 from paddleocr import PaddleOCR
 
 
-def extract_text(cropped_image):
-    # Initialize PaddleOCR
-    print("Initializing PaddleOCR...")
-    ocr = PaddleOCR(use_angle_cls=True, lang="en", show_log=True, use_gpu=True)
+class PaddleOCRReader:
+    def __init__(self):
+        # Initialize PaddleOCR
+        self.ocr = PaddleOCR(
+            use_angle_cls=False, lang="en", show_log=True, use_gpu=False
+        )
 
-    # Convert PIL image to RGB if it's not already
-    print("Converting image to RGB...")
-    converted_image = cropped_image.convert("RGB")
+    def extract_text(self, cropped_image):
+        # Convert PIL image to numpy array
+        print("Converting image to numpy array...")
+        image = np.array(cropped_image)
 
-    # Convert PIL image to numpy array
-    print("Converting image to numpy array...")
-    image = np.array(converted_image)
+        # Perform OCR on the image
+        print("Performing OCR on the image...")
+        result = self.ocr.ocr(image, cls=True)
 
-    # Perform OCR on the image
-    print("Performing OCR on the image...")
-    result = ocr.ocr(image, cls=True)
-
-    # Print the results
-    print(f"Got {len(result)} OCR results...")
-    for idx in range(len(result)):
-        res = result[idx]
-        for line in res:
-            print(line)
+        # Print the results
+        print(f"Got {len(result)} OCR results...")
+        for idx in range(len(result)):
+            res = result[idx]
+            for line in res:
+                print(line)
